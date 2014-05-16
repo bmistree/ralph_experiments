a34f36c4f1b1467c1023890ee022a0150db54f6e
bmistree <bmistree@gmail.com>
2014-05-16 13:34:50 -0700

    Issuing commands wrapped by perf.  Still need to parse perf output and collect it.

--- src/read_only/num_threads_tests.go
+++ src/read_only/num_threads_tests.go
@@ -5,29 +5,54 @@ import "path/filepath"
 import "strconv"
 
 const NUM_THREADS_OUTPUT_NAME = "num_threads.csv"
+const PERF_NUM_THREADS_OUTPUT_NAME = "num_threads.csv"
 const NUM_THREADS_TEST_NUM_READS = 100000
 var NUM_THREADS_TEST_NUM_THREADS [4]uint32 = [4]uint32{1,2,5,10}
 
 
 func numThreadsTests(readOnly* ReadOnly,jarDir,outputFolder string) {
+    outputFilename := filepath.Join(outputFolder,NUM_THREADS_OUTPUT_NAME)
+    commonNumThreadsTests(readOnly,jarDir,outputFilename,false)
+}
+
+
+func perfNumThreadsTests(readOnly* ReadOnly,jarDir,outputFolder string) {
+    outputFilename := filepath.Join(outputFolder,PERF_NUM_THREADS_OUTPUT_NAME)
+    commonNumThreadsTests(readOnly,jarDir,outputFilename,true)
+}
+
+func commonNumThreadsTests(
+    readOnly* ReadOnly,jarDir,outputFilename string, perfTest bool) {
     fqJar := filepath.Join(jarDir,READ_ONLY_JAR_NAME)
-    fmt.Println("Running num threads experiment: ");
+    if perfTest {
+        fmt.Println("Running perf num threads experiment: ")
+    } else {
+        fmt.Println("Running num threads experiment: ")
+    }
+    
     var results []ReadOnlyResult
     for i := 0; i < NUM_TIMES_TO_RUN_EACH_EXPERIMENT; i++ {
         fmt.Println(
             "\t" + strconv.Itoa(i+1) + " of " +
             strconv.Itoa(NUM_TIMES_TO_RUN_EACH_EXPERIMENT));
+        
         for _,numThreads := range NUM_THREADS_TEST_NUM_THREADS {
             // try thread size tests across all operation types
             for _, opType := range ALL_OPERATION_TYPES {
-                result := readOnly.readOnlyJar(
-                    fqJar,NUM_THREADS_TEST_NUM_READS,numThreads,opType)
+
+                var result ReadOnlyResult
+                if perfTest {
+                    result = readOnly.readOnlyJar(
+                        fqJar,NUM_THREADS_TEST_NUM_READS,numThreads,opType)
+                } else {
+                    result = readOnly.perfReadOnlyJar(
+                        fqJar,NUM_THREADS_TEST_NUM_READS,numThreads,opType)
+                }
                 results = append(results,result)
             }
         }
     }
     
     // write results to file
-    resultsToFile(
-        results,filepath.Join(outputFolder,NUM_THREADS_OUTPUT_NAME))
+    resultsToFile(results,outputFilename)
 }
--- src/read_only/read_only.go
+++ src/read_only/read_only.go
@@ -5,6 +5,7 @@ import "os/exec"
 import "log"
 import "bytes"
 import "strings"
+import "fmt"
 
 
 /**
@@ -31,7 +32,7 @@ const READ_ATOM_MAP_ARG = "-am"
 const READ_NUM_ARG = "-nan"
 const READ_MAP_ARG = "-nam"
 
-const NUM_TIMES_TO_RUN_EACH_EXPERIMENT = 10
+const NUM_TIMES_TO_RUN_EACH_EXPERIMENT = 2
 
 type ReadOnly struct {
 }
@@ -39,6 +40,36 @@ type ReadOnly struct {
 func(readOnly* ReadOnly) RunAll(jarDir,outputFolder string) {
     singleThreadWarmTests(readOnly,jarDir,outputFolder)
     numThreadsTests(readOnly,jarDir,outputFolder)
+    perfNumThreadsTests(readOnly,jarDir,outputFolder)
+}
+
+func (readOnly ReadOnly) perfReadOnlyJar(
+    fqJar string, numReads, numThreads uint32, opType operationType) ReadOnlyResult {
+    
+    argSlice := []string {"stat","java","-jar",fqJar}
+    argSlice = append(argSlice,readOnly.addReadsPerThread(numReads)...)
+    argSlice = append(argSlice,readOnly.addNumThreads(numThreads)...)
+    argSlice = append(argSlice,readOnly.addOperationType(opType)...)
+    
+    var stdOut bytes.Buffer
+    var stdErr bytes.Buffer
+    cmd := exec.Command("perf", argSlice...)
+    cmd.Stdout = &stdOut
+    cmd.Stderr = &stdErr
+	err := cmd.Run()
+	if err != nil {
+        log.Fatal(err)
+	}
+
+    outputString := stdOut.String()
+    perfStatsString := stdErr.String()
+
+    fmt.Println("\n\n")
+    fmt.Println(perfStatsString)
+    fmt.Println("\n\n")
+
+    return testRunOutputToResults(
+        outputString,numReads,numThreads,opType)
 }
 
 func (readOnly ReadOnly) readOnlyJar (
@@ -56,14 +87,19 @@ func (readOnly ReadOnly) readOnlyJar (
 	if err != nil {
         log.Fatal(err)
 	}
+    return testRunOutputToResults(
+        out.String(),numReads,numThreads,opType)
+}
 
-    cmdResult := strings.TrimSpace(out.String())
+func testRunOutputToResults(
+    cmdResult string,numReads,numThreads uint32, opType operationType) ReadOnlyResult {
     splitResults := strings.Split(cmdResult," ")
     if len(splitResults) != 2 {
         log.Fatal("Expected 2 results when splitting")
     }
 
-    opsPerSecond, err := strconv.ParseFloat(splitResults[1],64)
+    opsPerSecond, err :=
+        strconv.ParseFloat(strings.TrimSpace(splitResults[1]),64)
     if err != nil {
         log.Fatal(err)
     }
@@ -76,6 +112,7 @@ func (readOnly ReadOnly) readOnlyJar (
     return toReturn
 }
 
+    
 
 func (readOnly ReadOnly) addReadsPerThread(numOps uint32) []string {
     
