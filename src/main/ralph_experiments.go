0adf0d98c67e1f7d1f7383f92c557ecffa4e1601
bmistree <bmistree@gmail.com>
2014-05-16 10:08:33 -0700

    Added command line option to specify name of folder to output experiments to.

--- src/main/ralph_experiments.go
+++ src/main/ralph_experiments.go
@@ -4,10 +4,13 @@ import "os"
 import "log"
 
 func main() {
-    if len(os.Args) != 2 {
+    if len(os.Args) != 3 {
         log.Fatal("Require argument for read performance test jar dir")
     }
     readOnlyJarDir := os.Args[1]
+    outputDir := os.Args[2]
+    // create output directory if doesn't already exist
+    os.MkdirAll(outputDir,0777)
     readOnlyModule := read_only.ReadOnly{}
-    readOnlyModule.RunAll(readOnlyJarDir,"output_folder")
+    readOnlyModule.RunAll(readOnlyJarDir,outputDir)
 }
