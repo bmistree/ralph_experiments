4ead6f898fc67272bb8399586b7bf790c42fd03f
bmistree <bmistree@gmail.com>
2014-05-20 00:38:40 -0700

    Refactored plotting according to num threads to util.

--- disp/js/locks_off_tests_graph.js
+++ disp/js/locks_off_tests_graph.js
@@ -7,50 +7,6 @@ LOCKS_OFF_TESTS_DIV_ID = 'locks_off_tests';
  */
 function draw_locks_off_graph(data_list)
 {
-    // keys are number of operations performed, values are lists of
-    // floats.  Each float is measured throughput of that condition.
-    var locks_off_graph_data_dict = {};
-    // each element is the number of reads performed on a particular
-    // condition.
-    var num_threads_list = [];
-    for (var index in data_list)
-    {
-        var condition_data = new RunStats(data_list[index]);
-        if (! (condition_data.num_threads in locks_off_graph_data_dict))
-        {
-            locks_off_graph_data_dict[condition_data.num_threads] = [];
-            num_threads_list.push(condition_data.num_threads);
-        }
-        locks_off_graph_data_dict[condition_data.num_threads].push(condition_data);
-    }
-
-    // sort num_threads_list so that graph conditions will be displayed
-    // in order
-    num_threads_list.sort();
-
-    // flatten data into a single average.  Each element in this list
-    // is a RunStats object
-    var average_data_list = [];
-    
-    for (index in locks_off_graph_data_dict)
-    {
-        var avg_stats = stats_avg_throughput(locks_off_graph_data_dict[index]);
-        average_data_list.push(avg_stats);
-    }
-
-    // sort warm_graph_average_data_list by num_reads so that plot
-    // data in order.
-    average_data_list.sort(
-        function (a,b)
-        {
-            if (a.num_threads < b.num_threads)
-                return -1;
-            if (a.num_threads > b.num_threads)
-                return 1;
-            return 0;
-        });
-    
-    // Handle plotting averaged data
-    plot(LOCKS_OFF_TESTS_DIV_ID,average_data_list);
+    draw_num_threads_graph(data_list,LOCKS_OFF_TESTS_DIV_ID);
 }
 
--- disp/js/util.js
+++ disp/js/util.js
@@ -20,6 +20,56 @@ BAR_WIDTH = 40;
 BAR_CATEGORY_SPACING_WIDTH = 10;
 BAR_HEIGHT = 200;
 
+function draw_num_threads_graph(data_list,div_id)
+{
+    // keys are number of operations performed, values are lists of
+    // floats.  Each float is measured throughput of that condition.
+    var graph_data_dict = {};
+    // each element is the number of reads performed on a particular
+    // condition.
+    var num_threads_list = [];
+    for (var index in data_list)
+    {
+        var condition_data = new RunStats(data_list[index]);
+        if (! (condition_data.num_threads in graph_data_dict))
+        {
+            graph_data_dict[condition_data.num_threads] = [];
+            num_threads_list.push(condition_data.num_threads);
+        }
+        graph_data_dict[condition_data.num_threads].push(condition_data);
+    }
+
+    // sort num_threads_list so that graph conditions will be displayed
+    // in order
+    num_threads_list.sort();
+
+    // flatten data into a single average.  Each element in this list
+    // is a RunStats object
+    var average_data_list = [];
+    
+    for (index in graph_data_dict)
+    {
+        var avg_stats = stats_avg_throughput(graph_data_dict[index]);
+        average_data_list.push(avg_stats);
+    }
+
+    // sort warm_graph_average_data_list by num_reads so that plot
+    // data in order.
+    average_data_list.sort(
+        function (a,b)
+        {
+            if (a.num_threads < b.num_threads)
+                return -1;
+            if (a.num_threads > b.num_threads)
+                return 1;
+            return 0;
+        });
+    
+    // Handle plotting averaged data
+    plot(div_id,average_data_list);
+}
+
+
 /**
  @param {string} div_id_to_plot_on
  @param {list} data_list --- Each element is a RunStats obj
