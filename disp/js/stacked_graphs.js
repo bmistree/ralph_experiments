bea5fb0501ab976f6015f6759c7e178e36b44867
behram mistree <bmistree@gmail.com>
2014-05-22 12:38:24 -0700

    Rotations for text were incorrect.  Rotation command rotates around the axis specified by last two arguments.

--- disp/js/stacked_graphs.js
+++ disp/js/stacked_graphs.js
@@ -52,7 +52,7 @@ function draw_single_stacked_run(stacked_run,div_id_to_plot_on)
     var bar_chart = d3.select('#' + div_id_to_plot_on).
         append('svg:svg').
         attr('width', full_graph_width).
-        attr('height', STACKED_BAR_HEIGHT+60);
+        attr('height', STACKED_BAR_HEIGHT+100);
 
     // actually draw the bars
     bar_chart.selectAll('rect').
@@ -91,22 +91,25 @@ function draw_single_stacked_run(stacked_run,div_id_to_plot_on)
         attr('x',
              function(flattened_datum)
              {
-                 return x_rect_positions(flattened_datum.depth) - 10;
+                 // return x_rect_positions(flattened_datum.depth) - 10;
+                 return x_rect_positions(flattened_datum.depth);
              }).
         attr('y',
              function(flattened_datum)
              {
                  var returned_y =
-                     (STACKED_BAR_HEIGHT - y_heights(flattened_datum.end));
+                     (STACKED_BAR_HEIGHT - y_heights(flattened_datum.start));
                  return returned_y;
              }).
         attr('transform',
              function (flattened_datum)
              {
-                 var bar_height = y_heights(flattened_datum.end - flattened_datum.start);
-                 var y = (STACKED_BAR_HEIGHT - y_heights(flattened_datum.end) - bar_height);
-                 var x = x_rect_positions(flattened_datum.depth) - 10;
-                 return 'rotate(270,' + x + ',' + y + ')';
+                 // rotate: go to axis specified by x,y and rotate
+                 // this element degrees around it.
+                 var y = STACKED_BAR_HEIGHT - y_heights(flattened_datum.start);
+                 var x = x_rect_positions(flattened_datum.depth);
+                 var transform_to_perform = 'rotate(270,' + x + ',' + y + ')';
+                 return transform_to_perform;
              }).
         text(function(flattened_datum)
              {
